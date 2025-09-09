9931662bd94e8d08735062f3975e0abd62ac4f3c
dharmiki <dharmikiparmar@gmail.com>
2025-09-09 11:56:17 +0530

    Introduction

---
+++ Introduction/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>
+        Hi, I'm <span>Parmar Dharmiki V.</span> 👋
+      </h1>
+      <p>🚀 Front-End Developer</p>
+      <p>💻 HTML • CSS • JavaScript • Tailwind</p>
+      <p>✨ Passionate about clean UI & modern design</p>
+
+      <div className="btn-group">
+        <button className="btn btn-primary">View Projects</button>
+        <button className="btn btn-outline">Contact Me</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
